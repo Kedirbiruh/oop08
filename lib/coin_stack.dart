aab30bd05ed9c26b2b8bdb91b870e3bd2c83d61e
Kedir Biruh <kedirbiruh1114@gmail.com>
2025-10-01 10:10:10 +0200

    create Getter int get totalValue

--- lib/coin_stack.dart
+++ lib/coin_stack.dart
@@ -1,4 +1,22 @@
-
 class CoinStack {
-  final coins;
+  final List<int> _coins;
+
+  CoinStack(this._coins);
+
+  int get totalValue {
+    int sum = 0;
+    for (var coin in _coins) {
+      sum += _coin;
+    }
+    return sum;
+  }
+
+  bool operator >(CoinStack other) => this.totalValue > other.totalValue;
+  bool operator <(CoinStack other) => this.totalValue < other.totalValue;
+  bool operator >=(CoinStack other) => this.totalValue >= other.totalValue;
+  bool operator <=(CoinStack other) => this.totalValue <= other.totalValue;
+  bool operator ==( other) =>
+      other is CoinStack && this.totalValue == other.totalValue;
+
+  this CoinStack + (other CoinStack) => CoinStack([_coins, other._coins]);
 }
