3b84f2b4bf6b6e263c78083cd28ba03389d6239d
Kedir Biruh <kedirbiruh1114@gmail.com>
2025-10-01 15:09:05 +0200

    list unmodifiable

--- lib/coin_stack.dart
+++ lib/coin_stack.dart
@@ -6,9 +6,13 @@ class CoinStack {
   CoinStack(List<int> coins) : _coins = List.from(coins);
 
   // bennante konstruktor
-  CoinStack.from(CoinStack other) : _coins = List.from(other._coins); //erstellt eine Kopie von einem anderen CoinStack
+  CoinStack.from(CoinStack other)
+    : _coins = List.from(
+        other._coins,
+      ); //erstellt eine Kopie von einem anderen CoinStack
   // totalValue Berechnung
   int get totalValue => _coins.fold(0, (sum, coin) => sum + coin);
+  List<int> get coins => List.unmodifiable(_coins);
 
   // int get totalValue {
   //   int sum = 0;
@@ -27,8 +31,9 @@ class CoinStack {
   bool operator ==(Object other) =>
       other is CoinStack && totalValue == other.totalValue;
 
-
   //passt zu deiner == Implementierung, weil beide nur auf totalValue schauen.
+  //Geld arbeitest → vergleiche nach Wert (Summe).
+  //Münzsammler bist → vergleiche nach genauer Liste.
   @override
   int get hashCode => totalValue.hashCode; // wichtig, weil == überschrieben
 
@@ -38,7 +43,9 @@ class CoinStack {
   }
 
   CoinStack? operator -(CoinStack other) {
-    final newCoins = List<int>.from(_coins); //macht eine Kopie (List.from(coins)), damit man später die Original-Liste nicht mehr „von außen“ ändern kann.
+    final newCoins = List<int>.from(
+      _coins,
+    ); //macht eine Kopie (List.from(coins)), damit man später die Original-Liste nicht mehr „von außen“ ändern kann.
     for (final coin in other._coins) {
       if (newCoins.contains(coin)) {
         newCoins.remove(coin);
